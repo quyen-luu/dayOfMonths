59729c42e3ef58af318a3666e7616debf4d75044
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-14 23:11:41 +0700

    Add content

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhập tháng bạn cần kiểm tra ngày: ");
+        int month = Convert.ToInt32(Console.ReadLine());
+
+        switch (month)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+            Console.WriteLine($"Tháng {month} có 31 ngày.");
+            break;
+            case 2:
+            Console.WriteLine($"Tháng {month} có 28 hoặc 29 ngày.");
+            break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+            Console.WriteLine($"Tháng {month} có 30 ngày.");
+            break;
+            default:
+            Console.WriteLine("Vui lòng nhập lại.");
+            break;
+        }
+    }
+}
